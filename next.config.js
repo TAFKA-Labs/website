d05b9273daf255c3b13913aeb49c4626a8a6b2e9
allpwrfulroot <akunz@alum.mit.edu>
2019-10-10 15:28:01 -0400

    fix again

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 require('dotenv').config()
 const Dotenv = require('dotenv-webpack')
 const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS({
-  webpack: config => {
-    config.plugins = [
-      ...config.plugins,
-
-      new Dotenv({
-        path: './.env',
-        systemvars: true,
-      }),
-    ]
-
-    return config
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withCSS({
+    webpack: config => {
+      config.plugins = [
+        ...config.plugins,
+
+        new Dotenv({
+          path: './.env',
+          systemvars: true,
+        }),
+      ]
+
+      return config
+    },
+  })
+)
